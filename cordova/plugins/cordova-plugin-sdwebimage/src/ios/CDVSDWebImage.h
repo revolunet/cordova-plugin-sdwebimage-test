25a50af963be81bad814c4bf6b246beb14d0d74f
Julien Bouquillon <julien@bouquillon.com>
2015-09-30 14:17:46 +0200

    remove symlink

---
+++ cordova/plugins/cordova-plugin-sdwebimage/src/ios/CDVSDWebImage.h
@@ -0,0 +1,16 @@
+
+
+#import <Cordova/CDV.h>
+#import "UIImageView+WebCache.h"
+
+@interface CDVSDWebImage : CDVPlugin<SDWebImageManagerDelegate> {
+    NSDictionary *options;
+    NSMutableDictionary *indexes;
+    SDWebImageManager *manager;
+}
+
+- (void)getImage:(CDVInvokedUrlCommand*)command;
+- (void)clearCache:(CDVInvokedUrlCommand*)command;
+
+
+@end
